4bfe75d9937373f7651848a2c683127ab4140adb
Vladyslav Makarevych <vladyslav.makarevych@student.tuke.sk>
2023-10-30 22:23:00 +0100

    Update FE user registration, user login, user profile and home with updated BE API, Update connect FE app to BE, display hello-world API message

--- frontend-web/src/hooks/useHelloWorldAPI.ts
+++ frontend-web/src/hooks/useHelloWorldAPI.ts
@@ -1,11 +1,19 @@
 import { useState, useEffect } from 'react';
 import { HelloWorldResponse } from '../types/apiTypes';
+import { useAuth } from '../AuthProvider'; 
 
 function useHelloWorldAPI() {
   const [helloWorldText, setHelloWorldText] = useState('Trying to connect...');
+  const [error, setError] = useState<string | null>(null);
+
+  const { token } = useAuth(); 
 
   useEffect(() => {
-    fetch('https://frf-api.azurewebsites.net/api/HelloWorld')
+    fetch('https://frf-api.azurewebsites.net/api/HelloWorld', {
+      headers: {
+        Authorization: `Bearer ${token}`, 
+      },
+    })
       .then((response) => {
         if (!response.ok) {
           throw new Error(response.statusText);
@@ -16,11 +24,11 @@ function useHelloWorldAPI() {
         setHelloWorldText(data.message);
       })
       .catch((error) => {
-        setHelloWorldText(error);
+        setError(error.message);
       });
-  }, []);
+  }, [token]);
 
-  return helloWorldText;
+  return { helloWorldText, error };
 }
 
 export default useHelloWorldAPI;
--- frontend-web/src/pages/HomePage.tsx
+++ frontend-web/src/pages/HomePage.tsx
@@ -2,14 +2,21 @@ import { Container } from 'react-bootstrap';
 import useHelloWorldAPI from '../hooks/useHelloWorldAPI';
 
 function HomePage() {
-  const helloWorldText = useHelloWorldAPI();
-
+  const { helloWorldText, error } = useHelloWorldAPI();
+  if (error) {
+    return (
+      <Container>
+        <p>Error: {error}</p>
+      </Container>
+    );
+  }else{
   return (
     <Container>
       <h1>Connection test with BE by API call</h1>
       <p>{helloWorldText}</p>
     </Container>
   );
+  }
 }
 
 export default HomePage;
