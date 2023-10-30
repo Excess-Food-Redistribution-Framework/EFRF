d27113b2695180324d2f2befc88e68245da3deed
Raman <raman.listarevich@student.tuke.sk>
2023-10-30 15:19:55 +0300

    Fix createOrganozationDto (now not contains id)

--- backend/FRF.API/Dto/Organization/CreateOrganizationDto.cs
+++ backend/FRF.API/Dto/Organization/CreateOrganizationDto.cs
@@ -3,10 +3,21 @@ using System.ComponentModel.DataAnnotations;
 
 namespace FRF.API.Dto.Organization
 {
-    public class CreateOrganizationDto : OrganizationDto
+    public class CreateOrganizationDto
     {
         [Required]
         [Display(Name = "Password")]
         public string Password { get; set; } = string.Empty;
+
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [Display(Name = "Type")]
+        public OrganizationType Type { get; set; }
+
+        [Display(Name = "Information")]
+        public string Information { get; set; } = string.Empty;
     }
 }
