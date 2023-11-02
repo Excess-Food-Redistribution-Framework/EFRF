8667948ed29c4e1c8d6f306112f1f3d47a1706da
Lukáš Ondrejka <lukas.ondrejka@student.tuke.sk>
2023-11-02 13:39:07 +0100

    Fix registration redirect

--- frontend-web/src/pages/RegistrationPage.tsx
+++ frontend-web/src/pages/RegistrationPage.tsx
@@ -29,7 +29,7 @@ function RegistrationPage() {
       );
 
       setResponseMessage(response.data);
-      navigate('./Login');
+      navigate('/login');
     } catch (error) {
       console.error(error);
     }
