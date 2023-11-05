c152411148535bafea5785b48e4712eed514db94
Lukáš Ondrejka <lukas.ondrejka@student.tuke.sk>
2023-11-05 18:15:37 +0100

    Add Teaser to Article

--- backend/FRF.Domain/Entities/Article.cs
+++ backend/FRF.Domain/Entities/Article.cs
@@ -4,5 +4,6 @@ public class Article : BaseEntity
 {
     public Guid Id { get; set; }
     public string Title { get; set; } = string.Empty;
+    public string Teaser { get; set; } = string.Empty;
     public string Content { get; set; } = string.Empty;
 }
