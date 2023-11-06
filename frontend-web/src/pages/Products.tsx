5167262ebe47bd500f60b0ac7eadb867f9287292
Jakub Polaško <jakub.polasko@student.tuke.sk>
2023-11-06 15:46:35 +0100

    productCards update

--- frontend-web/src/pages/Products.tsx
+++ frontend-web/src/pages/Products.tsx
@@ -3,8 +3,8 @@ import ProductCards from '../components/ProductCards';
 
 function Product() {
   return (
-    <Container fluid>
-      <Container fluid className="px-0 secondary_color">
+    <Container fluid className="px-0">
+      <Container fluid className="secondary_color">
         <Row className="justify-content-center diagonal-bg p-5">
           <Col className="text-center d-flex flex-column justify-content-center">
             <h1 className="text-white text-shadow pb-2">Products</h1>
