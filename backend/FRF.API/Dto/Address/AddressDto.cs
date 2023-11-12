f5e0d49212bc7b917ac455826f0ba0c38f3738f5
Raman <raman.listarevich@student.tuke.sk>
2023-11-12 15:38:39 +0300

    Add location and address fields to organization

---
+++ backend/FRF.API/Dto/Address/AddressDto.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FRF.API.Dto.Address
+{
+    public class AddressDto
+    {
+        [Required]
+        [Display(Name = "State")]
+        public string State { get; set; } = String.Empty;
+
+        [Required]
+        [Display(Name = "City")]
+        public string City { get; set; } = String.Empty;
+
+        [Required]
+        [Display(Name = "Street")]
+        public string Street { get; set; } = String.Empty;
+
+        [Required]
+        [Display(Name = "Street")]
+        public string Number { get; set; } = String.Empty;
+
+
+        [Required]
+        [Display(Name = "ZipCode")]
+        public string ZipCode { get; set; } = String.Empty;
+    }
+}
