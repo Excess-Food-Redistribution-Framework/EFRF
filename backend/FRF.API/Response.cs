35c9134295299ca87baefbd2791d7fc0e2f7d2dc
Lukáš Ondrejka <lukas.ondrejka@student.tuke.sk>
2023-11-13 21:52:40 +0100

    Delete unused file

--- backend/FRF.API/Response.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FRF.API;
-
-public class Response<T>
-{
-    public int Status { get; set; } = 200;
-    public bool Success { get; set; } = true;
-    public string Message { get; set; } = string.Empty;
-    public T? Data { get; set; }
-}
