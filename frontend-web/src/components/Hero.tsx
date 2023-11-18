f056240ff4df2cab2268dc826ed6c5cb9f2b62dc
Tomáš Zajac <zajac@provys.com>
2023-11-18 16:23:20 +0100

    Moved Hero component Moved hero component to file Hero.tsx for cleaner code

---
+++ frontend-web/src/components/Hero.tsx
@@ -0,0 +1,77 @@
+import { Badge, Button, Col, Container, Image, Row } from 'react-bootstrap';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../AuthProvider';
+import ArticlesCards from "./ArticlesCards";
+
+function Hero() {
+  const { isAuth } = useAuth();
+  const navigate = useNavigate();
+  const handleClickButton = (path: string) => {
+    navigate(`./${path}`);
+  };
+  return (
+    <Container fluid className="secondary_color">
+      <Row className="justify-content-evenly diagonal-bg mh-400px p-5">
+        <Col xl="5" className="justify-content-center d-flex">
+          <Image src="/assets/img/hero.svg" className="img-fluid p-4 pb-0" />
+        </Col>
+        <Col
+          xl="3"
+          className="text-center d-flex flex-column justify-content-center py-5"
+        >
+          <h5 className="text-white text-shadow pb-2">
+            Give away unused <Badge bg="warning">Food</Badge>
+          </h5>
+          <h1 className="text-white text-shadow pb-2">
+            Helping each other can make world better
+          </h1>
+          <p className="text-white text-shadow pb-4">
+            We Seek out world changers and difference makers around the
+            globe,and equip them to fulfill their unique purpose.
+          </p>
+          {isAuth() ? (
+            <Row>
+              <Col>
+                <Button
+                  variant="primary"
+                  onClick={() => handleClickButton('profile')}
+                >
+                  Show Profile
+                </Button>
+              </Col>
+              <Col>
+                <Button
+                  variant="primary"
+                  onClick={() => handleClickButton('products')}
+                >
+                  Products
+                </Button>
+              </Col>
+            </Row>
+          ) : (
+            <Row>
+              <Col>
+                <Button
+                  variant="primary"
+                  onClick={() => handleClickButton('login')}
+                >
+                  Login
+                </Button>
+              </Col>
+              <Col>
+                <Button
+                  variant="primary"
+                  onClick={() => handleClickButton('registration')}
+                >
+                  Registration
+                </Button>
+              </Col>
+            </Row>
+          )}
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+export default Hero;
--- frontend-web/src/pages/HomePage.tsx
+++ frontend-web/src/pages/HomePage.tsx
@@ -1,79 +1,14 @@
-import {Badge, Button, Col, Container, Image, Row} from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../AuthProvider';
+import { Container } from 'react-bootstrap';
+import React from 'react';
 import ArticlesCards from '../components/ArticlesCards';
 import ProductCards from '../components/ProductCards';
-import React from "react";
+import Hero from '../components/Hero';
 
 function HomePage() {
-  const { isAuth } = useAuth();
-  const navigate = useNavigate();
-  const handleClickButton = (path: string) => {
-    navigate(`./${path}`);
-  };
   return (
     <Container fluid className="px-0">
-      <Container fluid className="secondary_color">
-        <Row className="justify-content-evenly diagonal-bg mh-400px p-5">
-          <Col xl="5" className="justify-content-center d-flex">
-            <Image src="/assets/img/hero.svg" className="img-fluid p-4 pb-0" />
-          </Col>
-          <Col
-            xl="3"
-            className="text-center d-flex flex-column justify-content-center py-5"
-          >
-            <h5 className="text-white text-shadow pb-2">
-              Give away unused <Badge bg="warning">Food</Badge>
-            </h5>
-            <h1 className="text-white text-shadow pb-2">
-              Helping each other can make world better
-            </h1>
-            <p className="text-white text-shadow pb-4">
-              We Seek out world changers and difference makers around the
-              globe,and equip them to fulfill their unique purpose.
-            </p>
-            {isAuth() ? (
-              <Row>
-                <Col>
-                  <Button
-                    variant="primary"
-                    onClick={() => handleClickButton('profile')}
-                  >
-                    Show Profile
-                  </Button>
-                </Col>
-                <Col>
-                  <Button
-                    variant="primary"
-                    onClick={() => handleClickButton('products')}
-                  >
-                    Products
-                  </Button>
-                </Col>
-              </Row>
-            ) : (
-              <Row>
-                <Col>
-                  <Button
-                    variant="primary"
-                    onClick={() => handleClickButton('login')}
-                  >
-                    Login
-                  </Button>
-                </Col>
-                <Col>
-                  <Button
-                    variant="primary"
-                    onClick={() => handleClickButton('registration')}
-                  >
-                    Registration
-                  </Button>
-                </Col>
-              </Row>
-            )}
-          </Col>
-        </Row>
-      </Container>
+      <Hero />
+
       <Container className="mt-4">
         <Container>
           <h5 style={{ display: 'inline' }}>Articles</h5>
