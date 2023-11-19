4e52ac5e0178967ae9f53b55b85468545fb576e8
Vladyslav Makarevych <vladyslav.makarevych@student.tuke.sk>
2023-11-19 21:25:07 +0100

    map with products card

---
+++ frontend-web/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer style={{ backgroundColor: '#2A2E45', color: '#fff', padding: '7px', textAlign: 'center' }}>
+      <p>&copy; 2023</p>
+    </footer>
+  );
+};
+
+export default Footer;
--- frontend-web/src/components/ProductsMap.tsx
+++ frontend-web/src/components/ProductsMap.tsx
@@ -1,54 +1,80 @@
-// ProductsMap.tsx
 import React, { useEffect, useState } from 'react';
 import { GoogleMap } from '@react-google-maps/api';
 import LoadMapContainer from '../components/LoadMapContainer';
 import { ProductsMapProps } from '../types/mapTypes';
 import { geocodeAddress } from '../utils/geocodeUtils';
+import ProductCards from '../components/ProductCards';
 
-const mapContainerStyle = {
-  width: '100%',
-  height: '71vh',
+const containerStyle: React.CSSProperties = {
+  display: 'flex',
+  flexDirection: 'row',
+};
+
+const mapContainerStyle: React.CSSProperties = {
+  flex: 1,
+  height: '100vh',
+};
+
+const cardsContainerStyle: React.CSSProperties = {
+  flex: 1,
+  padding: '20px',
 };
 
 function ProductsMap({ organizations }: ProductsMapProps) {
   const [map, setMap] = useState<google.maps.Map | null>(null);
   const [infoWindow, setInfoWindow] = useState<google.maps.InfoWindow | null>(null);
+  const [selectedOrganization, setSelectedOrganization] = useState<any>(null);
+  const [showDisabled] = useState<boolean>(true);
+  const [page] = useState<number>(1);
+  const [pageSize] = useState<number>(5);
+  const [isPagination] = useState<boolean>(true);
 
   useEffect(() => {
     const fetchLocations = async () => {
+     /* window.scrollTo({
+        top: window.innerHeight,
+        behavior: 'smooth',
+      });*/
       if (map && organizations.length > 0) {
         const bounds = new window.google.maps.LatLngBounds();
         const markers: google.maps.Marker[] = [];
 
         for (const organization of organizations) {
-          const { address } = organization;
+          if (organization.type === 'Provider') {
+            const { address } = organization;
 
-          if (address && address.street && address.number && address.city && address.state && address.zipCode) {
-            const fullAddress = `${address.street} ${address.number}, ${address.city}, ${address.state}, ${address.zipCode}`;
+            if (address && address.street && address.number && address.city && address.state && address.zipCode) {
+              const fullAddress = `${address.street} ${address.number}, ${address.city}, ${address.state}, ${address.zipCode}`;
 
-            try {
-              const result = await geocodeAddress(fullAddress);
+              try {
+                const result = await geocodeAddress(fullAddress);
 
-              if (result?.geometry?.location) {
-                const latLng = result.geometry.location;
+                if (result?.geometry?.location) {
+                  const latLng = result.geometry.location;
 
-                const circle = new window.google.maps.Circle({
-                  map,
-                  center: latLng,
-                  radius: 90,
-                  fillColor: 'blue',
-                  fillOpacity: 0.5,
-                  strokeColor: 'blue',
-                  strokeOpacity: 1,
-                  strokeWeight: 1,
-                });
+                  const circle = new window.google.maps.Circle({
+                    map,
+                    center: latLng,
+                    radius: 90,
+                    fillColor: 'blue',
+                    fillOpacity: 0.5,
+                    strokeColor: 'blue',
+                    strokeOpacity: 1,
+                    strokeWeight: 1,
+                  });
 
-                markers.push(new window.google.maps.Marker({ position: latLng, map }));
+                  const marker = new window.google.maps.Marker({ position: latLng, map });
+                  markers.push(marker);
 
-                bounds.extend(latLng);
+                  bounds.extend(latLng);
+
+                  marker.addListener('click', () => {
+                    showInfoWindow(organization, marker);
+                  });
+                }
+              } catch (error) {
+                console.error(error);
               }
-            } catch (error) {
-              console.error(error);
             }
           }
         }
@@ -56,16 +82,10 @@ function ProductsMap({ organizations }: ProductsMapProps) {
         map.addListener('click', () => {
           if (infoWindow) {
             infoWindow.close();
+            setSelectedOrganization(null);
           }
         });
 
-        markers.forEach((marker, index) => {
-          marker.addListener('click', () => {
-            showInfoWindow(organizations[index], marker);
-          });
-        });
-
-        console.log('Bounds:', bounds.toString());
         map.fitBounds(bounds);
       }
     };
@@ -75,7 +95,7 @@ function ProductsMap({ organizations }: ProductsMapProps) {
         infoWindow.close();
       }
 
-      const contentString = `<div><h3>${organization.name}</h3><p>${organization.address.city}, ${organization.address.state}</p></div>`;
+      const contentString = `<div><h3>${organization.name}</h3><p>${organization.address.street} ${organization.address.number}, ${organization.address.city}, ${organization.address.state}</p></div>`;
 
       const newInfoWindow = new window.google.maps.InfoWindow({
         content: contentString,
@@ -83,6 +103,7 @@ function ProductsMap({ organizations }: ProductsMapProps) {
 
       newInfoWindow.open(map!, marker);
       setInfoWindow(newInfoWindow);
+      setSelectedOrganization(organization);
     };
 
     if (map && organizations.length > 0) {
@@ -91,12 +112,39 @@ function ProductsMap({ organizations }: ProductsMapProps) {
   }, [map, organizations, infoWindow]);
 
   return (
-    <LoadMapContainer googleMapsApiKey="AIzaSyDs5b037pFZXoneZJqkYotM5XQvcKTWcNE">
-      <GoogleMap 
-        mapContainerStyle={mapContainerStyle}
-        onLoad={(map) => setMap(map)}
-      />
-    </LoadMapContainer>
+    <div style={containerStyle}>
+      <LoadMapContainer googleMapsApiKey="AIzaSyDs5b037pFZXoneZJqkYotM5XQvcKTWcNE">
+        <GoogleMap 
+          options={{
+            disableDefaultUI: true,
+            styles: [
+              {
+                featureType: 'poi',
+                elementType: 'labels',
+                stylers: [{ visibility: 'off' }],
+              },
+            ],
+          }}
+          mapContainerStyle={mapContainerStyle}
+          onLoad={(map) => setMap(map)}
+        />
+      </LoadMapContainer>
+      
+      {selectedOrganization && (
+        <div style={cardsContainerStyle}>
+          <ProductCards
+            params={{
+              page,
+              pageSize,
+              onlyAvailable: !showDisabled,
+              notExpired: !showDisabled,
+              organizationId: selectedOrganization.id,
+            }}
+            pagination={isPagination}
+          />
+        </div>
+      )}
+    </div>
   );
 }
 
