ad86549820153ff1574afb64590816ae3914c699
Lukáš Ondrejka <lukas.ondrejka@student.tuke.sk>
2024-01-03 14:28:55 +0100

    Add Support Tickets

---
+++ backend/FRF.Services/Implementations/TicketService.cs
@@ -0,0 +1,40 @@
+﻿using FRF.DAL.Interfaces;
+using FRF.Domain.Entities;
+using FRF.Services.Interfaces;
+
+namespace FRF.Services.Implementations;
+
+public class TicketService: ITicketService
+{
+    private readonly IBaseRepository<Ticket> _ticketRepository;
+    
+    public TicketService(IBaseRepository<Ticket>ticketRepository)
+    {
+        _ticketRepository = ticketRepository;
+    }
+
+    public async Task Add(Ticket item)
+    {
+        await _ticketRepository.Add(item);
+    }
+    
+    public async Task Delete(Guid id)
+    {
+        await _ticketRepository.Delete(id);
+    }
+    
+    public async Task<IQueryable<Ticket>> GetAll()
+    {
+        return _ticketRepository.GetAll();
+    }
+    
+    public async Task<Ticket> GetById(Guid id)
+    {
+        return await _ticketRepository.GetById(id);
+    }
+    
+    public async Task Update(Ticket item)
+    {
+        await _ticketRepository.Update(item);
+    }
+}
---
+++ backend/FRF.Services/Interfaces/ITicketService.cs
@@ -0,0 +1,12 @@
+﻿using FRF.Domain.Entities;
+
+namespace FRF.Services.Interfaces;
+
+public interface ITicketService
+{
+    Task<IQueryable<Ticket>> GetAll();
+    Task<Ticket> GetById(Guid id);
+    Task Add(Ticket item);
+    Task Update(Ticket item);
+    Task Delete(Guid id);
+}
